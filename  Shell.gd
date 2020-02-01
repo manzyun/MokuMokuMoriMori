942882016cda583aee47b12282cad2666a63162d
manzyun <manzyun@gmail.com>
2020-02-01 18:07:49 +0900

    [Mod] アリか単細胞生物のようなものが勝手に動くようにした。

---  Shell.gd
+++  Shell.gd
@@ -1,15 +1,30 @@
 extends Area2D
 
-export 
+signal collision_telitory
+signal collision_born
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var born = 3 # 誕生条件
+export var alive = 3 # 生存条件
+export var poor_die = 2 # 過疎死亡条件
+export var dense_die = 4 # 過密死亡条件
 
-# Called when the node enters the scene tree for the first time.
+export var per_move = 5 # 1フレームにつき移動する量
+
+var screen_size
+
 func _ready():
-	pass # Replace with function body.
+    screen_size = get_viewport_rect().size
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+        var verocity = Vector2()
+        verocity.x = rand_range(0, per_move)
+        verocity.y = rand_range(0, per_move)
+        verocity = verocity.normalized() * per_move
+
+        position += verocity * delta
+        position.x = clamp(position.x, 0, screen_size.x)
+        position.y = clamp(position.y, 0, screen_size.y)
+
+
+func _on_Shell_collision_telitory(shell0, shell1):
+    emit_signal("collision_telitory")
