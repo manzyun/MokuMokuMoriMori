febd7373920729fd3d8821cffd855ad3e52721a3
manzyun <manzyun@gmail.com>
2020-02-01 22:22:18 +0900

    [Mod] 駒？　が増えるぞ！！

---  Shell.gd
+++  Shell.gd
@@ -1,30 +1,30 @@
 extends Area2D
 
-signal collision_telitory
-signal collision_born
-
-export var born = 3 # 誕生条件
-export var alive = 3 # 生存条件
-export var poor_die = 2 # 過疎死亡条件
-export var dense_die = 4 # 過密死亡条件
+signal hit
+signal born
 
 export var per_move = 5 # 1フレームにつき移動する量
+export var life_point = 2
 
 var screen_size
+var tmp_collision_body
 
 func _ready():
     screen_size = get_viewport_rect().size
+    position.x = position.x * sin(randi())
+    position.y = position.y * cos(randi())
+    $TimerBorn.start(3)
 
 func _process(delta):
-        var verocity = Vector2()
-        verocity.x = rand_range(0, per_move)
-        verocity.y = rand_range(0, per_move)
-        verocity = verocity.normalized() * per_move
+    var verocity = Vector2()
+    verocity.x = rand_range(0, per_move)
+    verocity.y = rand_range(0, per_move)
+    verocity = verocity.normalized() * per_move
 
-        position += verocity * delta
-        position.x = clamp(position.x, 0, screen_size.x)
-        position.y = clamp(position.y, 0, screen_size.y)
+    position += verocity * delta
+    position.x = clamp(position.x, 0, screen_size.x)
+    position.y = clamp(position.y, 0, screen_size.y)
 
 
-func _on_Shell_collision_telitory(shell0, shell1):
-    emit_signal("collision_telitory")
+func _on_TimerBorn_timeout():
+    emit_signal("born")
--- Main.gd
+++ Main.gd
@@ -2,16 +2,16 @@ extends Node
 
 export (PackedScene) var Shell
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    randomize()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #    pass
+
+
+func _on_Shell_born():
+    var shell = Shell.instance()
+    add_child(shell)
