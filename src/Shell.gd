02276c0fed171c9bac27a0ca76944c7abe8aa3f7
AIPON3 <60562314+AIPON3@users.noreply.github.com>
2020-02-02 13:44:08 +0900

    [Mod] Super Cool KOBAE!! (#1)
    Co-authored-by: TAKAHASHI Hidetsugu <manzyun@gmail.com>

--- src/Shell.gd
+++ src/Shell.gd
@@ -1,18 +1,17 @@
 extends Area2D
 
-export var per_move = 100 # 1フレームにつき移動する量
-
-var screen_size: Vector2
+export var per_move = 20 # 1フレームにつき移動する量
 
 func _ready():
     screen_size = get_viewport_rect().size
     position += Vector2(rand_range(-10, 10), rand_range(-10, 10))
 
 func _process(delta):
-    position += (Vector2(clamp(rand_range(-per_move, per_move), 0,
-                                screen_size.x),
-                        clamp(rand_range(-per_move, per_move), 0,
-                                screen_size.y))) * delta
+    position += (Vector2(rand_range(-per_move, per_move),
+                        rand_range(-per_move, per_move)
+                        ) * per_move) * delta
+    position = Vector2(clamp(position.x, 0, screen_size.x),
+                clamp(position.y, 0, screen_size.y))
 
 func __BornPosition(n0: Area2D, n1: Area2D) -> Vector2:
     return Vector2((position.x + n0.position.x + n1.position.x) / 3,
