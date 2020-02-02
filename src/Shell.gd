9e2c5c793262fc74832e741359c1d482915860ce
manzyun <manzyun@gmail.com>
2020-02-02 13:03:44 +0900

    [WIP] 駒の粋が良くなった

--- src/Shell.gd
+++ src/Shell.gd
@@ -1,19 +1,18 @@
 extends Area2D
 
-export var per_move = 5 # 1フレームにつき移動する量
+export var per_move = 100 # 1フレームにつき移動する量
 
 var screen_size: Vector2
 
 func _ready():
     screen_size = get_viewport_rect().size
-    position = Vector2(rand_range(-10, 10), rand_range(-10, 10))
+    position += Vector2(rand_range(-10, 10), rand_range(-10, 10))
 
 func _process(delta):
-    position += (Vector2(rand_range(-per_move, per_move),
-                        rand_range(-per_move, per_move)
-                        ).normalized() * per_move) * delta
-    position = Vector2(clamp(position.x, 0, screen_size.x),
-                clamp(position.y, 0, screen_size.y))
+    position += (Vector2(clamp(rand_range(-per_move, per_move), 0,
+                                screen_size.x),
+                        clamp(rand_range(-per_move, per_move), 0,
+                                screen_size.y))) * delta
 
 func __BornPosition(n0: Area2D, n1: Area2D) -> Vector2:
     return Vector2((position.x + n0.position.x + n1.position.x) / 3,
